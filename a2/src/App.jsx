2f8ad6e3a82c497656a80df239a0fe9d9f8ab005
Archie <110494298+archie122@users.noreply.github.com>
2024-04-07 10:23:38 -0600

    Got Tailwind Working

--- a2/src/App.jsx
+++ a2/src/App.jsx
@@ -8,12 +8,12 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='flex place-content-center'>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo flex-auto" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react flex-auto" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
